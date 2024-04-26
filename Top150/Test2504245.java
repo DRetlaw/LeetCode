34507a6fa78aaa1ba26c318ba8854b10b5ecf606
Walter Dsouza <walterdsouza@in.ibm.com>
2024-04-26 11:51:15 +0530

    medium

---
+++ Top150/Test2504245.java
@@ -0,0 +1,45 @@
+public class Test2504245 {
+
+    public static void main(String[] args) {
+        //int [] nums = {0,0,1,1,1,2,2,3,3,4};
+        int [] nums = {0,0,1,1,1,1,2,3,3};
+        System.out.println(removeDuplicates(nums));
+        System.out.println("Done!!!");
+    }
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length == 1)
+            return 1;
+        int counter=1;
+        int current = nums[0];
+        int index = 0;
+        boolean secondOccurence = true;
+
+        for(int i=1;i<nums.length;i++)
+        {
+            if(current!=nums[i])
+            {
+                nums[++index]=nums[i];
+                current = nums[i];
+                counter++;
+                secondOccurence = true;
+            }
+            else
+            {
+                
+                if(secondOccurence)
+                {
+                    secondOccurence = false;
+                    nums[++index]=nums[i];
+                    counter++;
+                }
+            }
+            
+            
+        }
+
+        for(int i=0;i<nums.length;i++)
+            System.out.print(nums[i]+",");
+        return counter;
+    }
+    
+}
